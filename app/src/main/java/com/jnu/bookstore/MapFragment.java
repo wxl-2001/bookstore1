63e9b05ff76542c5059b01ad311c5b156c3ded1e
GitHub账号 <GitHub>
2021-12-05 14:34:08 +0800

    网络数据获取、解析及显示 12月5日

--- app/src/main/java/com/jnu/bookstore/MapFragment.java
+++ app/src/main/java/com/jnu/bookstore/MapFragment.java
@@ -1,14 +1,22 @@
 package com.jnu.bookstore;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapStatus;
@@ -20,6 +28,19 @@ import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.map.TextOptions;
 import com.baidu.mapapi.model.LatLng;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MapFragment#newInstance} factory method to
@@ -28,6 +49,7 @@ import com.baidu.mapapi.model.LatLng;
 public class MapFragment extends Fragment {
 
 
+    public static final int WHAT_DATA_OK = 1000;
     private MapView mapView;
 
     public MapFragment() {
@@ -41,11 +63,10 @@ public class MapFragment extends Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-        }
     }
 
     @Override
@@ -62,15 +83,82 @@ public class MapFragment extends Fragment {
         mapView.getMap().setMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
 
         BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.drawable.jinan);
-        MarkerOptions markerOption = new MarkerOptions().icon(bitmap).position(centerPoint);
-        Marker marker = (Marker) mapView.getMap().addOverlay(markerOption);
-        OverlayOptions textOption = new TextOptions().bgColor(0xAAFFFF00).fontSize(50)
-                .fontColor(0xFFFF00FF).text("我的学校").rotate(0).position(centerPoint);
-        mapView.getMap().addOverlay(textOption);
+
+        Handler handler=new Handler(Looper.getMainLooper())
+        {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+                if(msg.what==WHAT_DATA_OK)
+                {
+                    String content= msg.getData().getString("data");
+                    if(null!=content) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(content);
+                            JSONArray shops=jsonObject.getJSONArray("shops");
+                            for(int index=0;index<shops.length();index++)
+                            {
+                                JSONObject shop=shops.getJSONObject(index);
+
+                                LatLng centerPoint = new LatLng(shop.getDouble("latitude"),shop.getDouble("longitude"));
+                                MarkerOptions markerOption = new MarkerOptions().icon(bitmap).position(centerPoint);
+                                Marker marker = (Marker) mapView.getMap().addOverlay(markerOption);
+                                OverlayOptions textOption = new TextOptions().bgColor(0xAAFFFF00).fontSize(50)
+                                        .fontColor(0xFFFF00FF).text(shop.getString("name")).rotate(0).position(centerPoint);
+                                mapView.getMap().addOverlay(textOption);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                }
+
+            }
+        };
+        Runnable runnable=new Runnable(){
+
+            @Override
+            public void run() {
+                try {
+                    URL url=new URL("http://file.nidama.net/class/mobile_develop/data/bookstore.json");
+                    HttpURLConnection httpURLConnection=(HttpURLConnection)url.openConnection();
+                    httpURLConnection.setUseCaches(false);
+                    httpURLConnection.connect();
+                    if(httpURLConnection.getResponseCode()==HttpURLConnection.HTTP_OK)
+                    {
+                        InputStream inputStream= httpURLConnection.getInputStream();
+                        InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+                        BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+
+                        String line="";
+                        StringBuffer stringBuffer=new StringBuffer();
+                        while( null!=(line=bufferedReader.readLine())){
+                            stringBuffer.append(line);
+                        }
+                        Message message=new Message();
+                        message.what= WHAT_DATA_OK;
+                        Bundle bundle=new Bundle();
+                        bundle.putString("data",stringBuffer.toString());
+                        message.setData(bundle);
+
+                        handler.sendMessage(message);
+                        Log.i("test", "onCreateView: "+stringBuffer.toString());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(runnable).start();
+
         mapView.getMap().setOnMarkerClickListener(maker -> {
             Toast.makeText(getContext(), "Marker被点击了！", Toast.LENGTH_SHORT).show();
             return false;
         });
+
+
         return rootView;
     }
     @Override
